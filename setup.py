381da62681132f16f41c676ce60a36747e2ef873
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2014-05-05 14:23:48 -0300

    Update to alpha release (need to cover 100% before any release)

--- setup.py
+++ setup.py
@@ -16,11 +16,11 @@ setup(name="ESTester",
       download_url = 'http://pypi.python.org/pypi/ESTester',
       description=u"Utilities for testing ElasticSearch queries",
       include_package_data=True,
-      install_requires=["requests==2.0.0"],
+      install_requires=["requests>=2.0.0"],
       license="GNU GPLv2",
       long_description=README,
       packages=find_packages(),
       tests_require=["coverage==3.6", "nose==1.2.1", "pep8==1.4.1", "mock==1.0.1", "pylint==1.0.0"],
       url = "http://github.com/tatiana/estester",
-      version="1.2.0"
+      version="1.3.0a"
 )
